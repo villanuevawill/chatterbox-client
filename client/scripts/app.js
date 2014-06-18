d39253d962dd015076feb8cd3bf2938489f0b46c
William Villanueva <wvillanu89@gmail.com>
2014-06-17 22:35:00 -0700

    Finished all the requirements. Styling is the only step left.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,72 +2,110 @@
 //
 
 var app = {
+  id: 0,
+
+  friends: {},
+
+  username: null,
+
+  chatroom: 'lobby',
+
+  lastDate: null,
+
+  allRooms: [],
+
   init: function() {
     app.username = window.location.search.slice(10);
     // ajax call - get a list of rooms => limit 1000, order: -createdAt
-    var rooms = {};
-
-
     $.ajax({
       type: 'GET',
       url: 'https://api.parse.com/1/classes/chatterbox/',
       data: {order: '-createdAt', limit: 1000},
       contentType: 'application/JSON',
       success: function(data) {
-        if (data.results) {
-          data.results.forEach(function(result) {
-            //success => find unique room names
-            var thisRoom = result.roomname;
-            if(!rooms.hasOwnProperty(thisRoom)) {
-              rooms[thisRoom] = true;
-            }
-          });
-
-          for(var room in rooms) {
-            app.allRooms.push(room);
-          }
-          var check = 0;
-          app.allRooms.forEach(function(room){
-            $option = $('<option>',{value:room}).text(room);
-            if (room === app.chatroom){
-              $option.attr('selected','');
-              check = 1;
-            }
-            $('select').append($option);
-          });
-          if(check === 0){
-            app.chatroom = app.allRooms[0];
-          }
-          $('.chatroom').append('p').text(app.chatroom);
+          app.populateRooms(data.results);
+          app.renderRoomSelection()
           var d = new Date();
           d.setTime(d.getTime()-10000000);
-          app.getNewMessages(app.chatroom, d);
+          app.fetch(app.chatroom, d);
           app.refresh();
-          $('.sendButton').on('click', function(event) {
-            var message = $('.messages').val();
-            app.send(app.createMessage(message));
-          });t
-          $('select').change(function(e){
-            app.chatroom = $('select option:selected').text();
-            $('.chatroom').text(app.chatroom);
-            app.breakRefresh();
-            $('.chatMessages').empty();
-            d = new Date();
-            d.setTime(d.getTime()-10000000);
-            app.lastDate = d;
-            app.getNewMessages(app.chatroom, app.lastDate);
-            app.refresh();
-          });
-          $('.sendUser').on('click', function(event) {
-            var user = $('.username').val();
-            app.username = user;
-            $('.username').val('');
-          });
-          $('.')
-        }
+          $('.sendButton').on('click',app.sendMessage);
+          $('select').change(app.changeChatroom);
+          $('.sendUser').on('click', app.newUser);
+          $('.sendChatroom').on('click', app.addChatroom);
       }
     });
 
+  },
+
+  friend: function(user){
+    var user = $(this).attr('class');
+    app.friends[user] = true;
+    $('.'+user).attr('class','bold');
+  },
+
+  addChatroom: function(){
+    chatName = $('.currentChatroom').val();
+    app.chatroom = chatName;
+    app.allRooms.push(chatName);
+    $option = $('<option>',{value:chatName}).text(chatName);
+    $('select').append($option);
+    $option.attr('selected','');
+    app.changeChatroom();
+
+  },
+
+  newUser: function(){
+    var user = $('.username').val();
+    app.username = user;
+    $('.username').val('');
+  },
+
+  changeChatroom: function(){
+   app.chatroom = $('select option:selected').text();
+    $('.chatroom').text(app.chatroom);
+    app.breakRefresh();
+    $('.chatMessages').empty();
+    d = new Date();
+    d.setTime(d.getTime()-10000000);
+    app.lastDate = d;
+    app.fetch(app.chatroom, app.lastDate);
+    app.refresh();
+  },
+
+  sendMessage: function(){
+    var message = $('.messages').val();
+    app.send(app.createMessage(message));
+  },
+
+  populateRooms: function(messages){
+    var rooms = {};
+    messages.forEach(function(result) {
+    var thisRoom = result.roomname;
+      if(!rooms.hasOwnProperty(thisRoom)) {
+        rooms[thisRoom] = true;
+      }
+    });
+
+    for(var room in rooms) {
+      app.allRooms.push(room);
+    }
+  },
+
+  renderRoomSelection: function(){
+    var check = 0;
+    app.allRooms.forEach(function(room){
+      $option = $('<option>',{value:room}).text(room);
+      if (room === app.chatroom){
+        $option.attr('selected','');
+        check = 1;
+      }
+        $('select').append($option);
+    });
+    if(check === 0){
+    app.chatroom = app.allRooms[0];
+    }
+    $('.chatroom').append('p').text(app.chatroom);
   },
 
   reload: null,
@@ -78,33 +116,21 @@ var app = {
 
   refresh: function(){
     app.reload = setInterval(function(){
-        app.getNewMessages(app.chatroom, app.lastDate);
-      }, 500);
+        app.fetch(app.chatroom, app.lastDate);
+      }, 1000);
   },
 
-
-  username: null,
-
-  chatroom: 'lobby',
-
-  lastDate: null,
-
-  allRooms: [],
-
-
-
-  getNewMessages: function(room, lastDate) {
+  fetch: function(room, lastDate) {
     var wherestring = {roomname: room,
                       createdAt: {$gte: {__type: 'Date',
                                    iso: lastDate.toISOString() }}};
-
     $.ajax({
       type: 'GET',
       url: 'https://api.parse.com/1/classes/chatterbox/',
       data: {where: JSON.stringify(wherestring), order: '-createdAt', limit: 10},
       contentType: 'application/JSON',
       success: function(data) {
-        if (data.results) {
+        if (data.results.length > 0) {
           data.results.reverse().forEach(function(result) {
             app.displayMessage(app.parseMessage(result));
           });
@@ -127,8 +153,15 @@ var app = {
     var text = message.text;
     var time = message.time;
     var username = message.username;
-    var display = '<li>' + time + '- ' + username + ': ' + text + '</li>';
-    $('.chatMessages').prepend(display);
+    var $display = $('<li>')
+    $display.attr('class',username);
+    if (username in app.friends){
+      $display.attr('class','bold');
+    }
+    $display.attr('id',++app.id);
+    $display.text(username+' ' + time + '- ' + username + ': ' + text)
+    $('.chatMessages').prepend($display);
+    $('#'+app.id).on('click', app.friend);
   },
 
   send: function(message){
