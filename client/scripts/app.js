cbefc7fcac71668d332483b8c569a38b93128827
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-17 17:31:51 -0700

    Implemented working version of chat application.  Chatroom creation left to finish.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,34 +3,113 @@
 
 var app = {
   init: function() {
-    // initial populate
-    // set last date
+    app.username = window.location.search.slice(10);
+    // ajax call - get a list of rooms => limit 1000, order: -createdAt
+    var rooms = {};
+
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.parse.com/1/classes/chatterbox/',
+      data: {order: '-createdAt', limit: 1000},
+      contentType: 'application/JSON',
+      success: function(data) {
+        if (data.results) {
+          data.results.forEach(function(result) {
+            //success => find unique room names
+            var thisRoom = result.roomname;
+            if(!rooms.hasOwnProperty(thisRoom)) {
+              rooms[thisRoom] = true;
+            }
+          });
+
+          for(var room in rooms) {
+            app.allRooms.push(room);
+          }
+          var check = 0;
+          app.allRooms.forEach(function(room){
+            $option = $('<option>',{value:room}).text(room);
+            if (room === app.chatroom){
+              $option.attr('selected','');
+              check = 1;
+            }
+            $('select').append($option);
+          });
+          if(check === 0){
+            app.chatroom = app.allRooms[0];
+          }
+          $('.chatroom').append('p').text(app.chatroom);
+          var d = new Date();
+          d.setTime(d.getTime()-10000000);
+          app.getNewMessages(app.chatroom, d);
+          app.refresh();
+          $('.sendButton').on('click', function(event) {
+            var message = $('.messages').val();
+            app.send(app.createMessage(message));
+          });
+          $('select').change(function(e){
+            app.chatroom = $('select option:selected').text();
+            $('.chatroom').text(app.chatroom);
+            app.breakRefresh();
+            $('.chatMessages').empty();
+            d = new Date();
+            d.setTime(d.getTime()-10000000);
+            app.lastDate = d;
+            app.getNewMessages(app.chatroom, app.lastDate);
+            app.refresh();
+          });
+          $('.sendUser').on('click', function(event) {
+            var user = $('.username').val();
+            app.username = user;
+            $('.username').val('');
+          });
+        }
+      }
+    });
 
   },
 
-  username: 'bob',
+  reload: null,
+
+  breakRefresh: function(){
+    clearInterval(app.reload);
+  },
+
+  refresh: function(){
+    app.reload = setInterval(function(){
+        app.getNewMessages(app.chatroom, app.lastDate);
+      }, 500);
+  },
+
+
+  username: null,
 
   chatroom: 'lobby',
 
-  lastDate: new Date().toISOString(),
+  lastDate: null,
 
-  get: function(room, lastDate) {
-    var wherestring = {roomname: room , createdAt: {$gte: {__type: 'Date', iso: lastDate }}};
+  allRooms: [],
+
+
+
+  getNewMessages: function(room, lastDate) {
+    var wherestring = {roomname: room,
+                      createdAt: {$gte: {__type: 'Date',
+                                   iso: lastDate.toISOString() }}};
 
     $.ajax({
       type: 'GET',
       url: 'https://api.parse.com/1/classes/chatterbox/',
       data: {where: JSON.stringify(wherestring), order: '-createdAt', limit: 10},
-      contentType: "application/json",
+      contentType: 'application/JSON',
       success: function(data) {
         if (data.results) {
-          data.results.forEach(function(result) {
+          data.results.reverse().forEach(function(result) {
             app.displayMessage(app.parseMessage(result));
           });
+          app.lastDate = new Date();
         }
       }
-
-
     });
   },
 
@@ -48,44 +127,45 @@ var app = {
     var time = message.time;
     var username = message.username;
     var display = '<li>' + time + '- ' + username + ': ' + text + '</li>';
-    $('.chatMessages').append(display);
+    $('.chatMessages').prepend(display);
   },
 
-  // send: function(message){
-  //   $.ajax({
-  //   // always use this url
-  //     url: 'https://api.parse.com/1/classes/chatterbox/',
-  //     type: 'POST',
-  //     data: JSON.stringify({username: message.username,
-  //            text: message.text,
-  //            roomname: message.roomname}),
-  //     contentType: 'application/json',
-  //     success: function (data) {
-  //       console.log(data);
-  //     },
-  //     error: function (data) {
-  //     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  //       console.error('chatterbox: Failed to send message');
-  //     }
-  //   });
-  // },
+  send: function(message){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox/',
+      type: 'POST',
+      data: JSON.stringify({username: message.username,
+             text: message.text,
+             roomname: message.roomname}),
+      contentType: 'application/json',
+      success: function (data) {
+      },
+      error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
 
 
-  createMessage: function(response) {
+  createMessage: function(message) {
+    return {
+      username: app.username,
+      text: message,
+      roomname: app.chatroom
+    };
 
   },
 
 };
 
 $(function() {
-  app.get('lobby', app.lastDate);
-  $('.sendButton').on('click', function(event) {
-    var message = $('.messages').val();
-    createMessage(message);
-  });
 
-  // app.send('mitch', 'HOLA', 'will');
-  // app.displayMessage({text:'hola', time: 'today', username: 'mitch'});
+
+  app.init();
+
+
+  // app.get('lobby', app.lastDate);
 
 });
 
